b596f22185fd36cbd08610f36bb9c5f45a518a05
Achal Vijayvargiya <achalvj@gmail.com>
2025-05-15 20:23:36 +0530

    chaning api key

--- app/utils/llm_models.py
+++ app/utils/llm_models.py
@@ -2,8 +2,8 @@ from langchain_groq import ChatGroq
 from langchain_google_genai import ChatGoogleGenerativeAI
 import streamlit as st
 
-model = ChatGroq(model_name="llama-3.3-70b-versatile",api_key = st.secrets["GROQ_API_KEY"])
-googleModel = ChatGoogleGenerativeAI(model="gemini-2.0-flash",google_api_key=st.secrets["GEMINI_API_KEY"])
+model = ChatGroq(model_name="llama-3.3-70b-versatile",api_key = "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")
+googleModel = ChatGoogleGenerativeAI(model="gemini-2.0-flash",google_api_key="AIzaSyAnoVyE7iAJh0D0fiKiwqFOO3KvqlEQ2aQ")
 
 def get_llm_model():
-    return googleModel; 
+    return model; 
